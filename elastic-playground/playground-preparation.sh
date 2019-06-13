0c2042a17e3e28d2e5de0929811567b6adf235ad
Arnold van Wijnbergen <avwinbox@CANDYBOX.local>
2019-06-13 14:38:01 +0200

    Updated

--- elastic-playground/playground-preparation.sh
+++ elastic-playground/playground-preparation.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+status=1
 kubectl apply -f https://download.elastic.co/downloads/eck/0.8.0/all-in-one.yaml
-kubectl -n elastic-system logs statefulset.apps/elastic-operator | grep "starting the webhookserver" | while read line
+while [ $status -ne 0 ]
 do
-  kubectl apply -f course/elasticsearch.yaml
-  kubectl apply -f course/kibana.yaml
-  kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
-  kubectl proxy --address=0.0.0.0 --accept-hosts='^.*$' &
+  kubectl -n elastic-system logs statefulset.apps/elastic-operator | grep "starting the webhookserver" 
+  status=$?
+  echo $status
 done
+kubectl apply -f course/elasticsearch.yaml
+kubectl apply -f course/kibana.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+kubectl proxy --address=0.0.0.0 --accept-hosts='^.*$' &
